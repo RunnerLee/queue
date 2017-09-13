75e1eff92baf7cf79c7e4d2fc1d531835e227763
RunnerLee <runnerleer@gmail.com>
2017-09-13 23:25:27 +0800

    update

---
+++ tests/Runner/Queue/Testing/JobTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @auth: RunnerLee
+ * @email: runnerleer@gmail.com
+ * @website: https://runnerlee.com
+ * @time: 2017-09
+ */
+
+namespace Runner\Queue\Testing;
+
+use Runner\Queue\Job;
+
+class JobTest extends \PHPUnit_Framework_TestCase
+{
+
+}
